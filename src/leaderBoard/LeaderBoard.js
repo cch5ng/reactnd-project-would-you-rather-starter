9d567f437c22bf5ef812109df165f17fdf90a08c
Carol Chung <cch5ng@gmail.com>
2018-04-12 14:13:03 -0700

    feat #5: logic to display leaderboard data

--- src/leaderBoard/LeaderBoard.js
+++ src/leaderBoard/LeaderBoard.js
@@ -1,57 +1,65 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-import './App.css';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import '../App.css';
 
 class LeaderBoard extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      loginUser: '',
-      isLoggedIn: false
-    }
-
-    this.onChangeHandler = this.onChangeHandler.bind(this);
-  }
-
-  onChangeHandler(ev) {
-    let value = ev.target.value;
-    let stateChange = {};
-    stateChange[ev.target.name] = value;
-    this.setState(stateChange, function() {
-      if (this.state.loginUser.length) {
-        // change state to logged in
-        console.log('user is logged in');
-      }
-    });
   }
 
   render() {
-    //const post = this.props.post
-    return (
-      <div className="header title">
-        <div className="header-main">
-          <h2><Link to="/" className="header-main">Would You Rather</Link></h2>
-        </div>
-        <div className="header-contact">
-          <p className="contact">
-            <Link to="/">Questions</Link>
-            <Link to="/leaderboard">Leader Board</Link>
-            <span>User-name and Logout</span>
-            <span>Select list for user names</span>
-          </p>
-          <select value={this.state.loginUser} onChange={this.onChangeHandler} name="loginUser">
-            <option value="">Login</option>
-            <option value="sarahedo">Sarah Edo</option>
-            <option value="tylermcginnis">Tyler McGinnis</option>
-            <option value="johndoe">John Doe</option>
-          </select>
-        </div>
+    const {login, users} = this.props;
+    let isLoggedIn;
+    let usersAr;
 
+    if (login && login.isLoggedIn) {
+      isLoggedIn = login.isLoggedIn;
+    }
+
+    if (users && users.users) {
+      usersAr = Object.keys(users.users).map(uid => users.users[uid]);
+      usersAr.sort((a, b) => {
+        let bNumAnswers = Object.keys(b.answers).length;
+        let aNumAnswers = Object.keys(a.answers).length;
+        return bNumAnswers - aNumAnswers;
+      });
+
+    }
+
+    return (
+      <div className="">
+        <h2>Leader Board</h2>
+
+        {isLoggedIn && (
+          usersAr.map(user => {
+            let numQuestionsAnswered = Object.keys(user.answers).length;
+
+            return (
+              <div key={user.id}>
+                <p>{user.name}</p>
+                <p><img src={user.avatarURL} alt="user avatar" /></p>
+                <p># Questions answered: {numQuestionsAnswered}</p>
+                <p># Questions asked: {user.questions.length}</p>
+              </div>
+            )
+          })
+        )}
+
+        {!isLoggedIn && (
+          <div>Sorry, you need to log in to view this page.</div>
+        )}
 
       </div>
     )
   }
 }
 
-export default LeaderBoard;
+function mapStateToProps({ login, users }) {
+  return {
+    login,
+    users,
+  }
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
